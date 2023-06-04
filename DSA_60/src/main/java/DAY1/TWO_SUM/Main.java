94f4a11b33c775072416a5634130ca4550dbc276
gsharma207 <saraswat948@gmail.com>
2023-06-04 16:34:19 +0530

    Brute Force Approach

--- DSA_60/src/main/java/DAY1/TWO_SUM/Main.java
+++ DSA_60/src/main/java/DAY1/TWO_SUM/Main.java
@@ -1,40 +1,37 @@
 package DAY1.TWO_SUM;
 
-/*Given an array of integers, find two numbers such that they add up to a specific target number.
-  The function twoSum should return indices of the two numbers such that they add up to the target,
-  where index1 < index2. Please note that your returned answers (both index1 and index2 ) are not zero-based.
-  Put both these numbers in order in an array and return the array from your function ( Looking at the function
-  signature will make things clearer ). Note that, if no pair exists, return an empty list.
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-  If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the
-  minimum index2, choose the one with minimum index1 out of them.
-        Problem Constraints
-        1 <= |A| <= 105
-        -10^8 <= Ai <= 10^8
-        -10^8 <= B <= 10^8
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
 
 
-        Input Format
-        The first argument is an integer array A.
-        The second argument is an integer B.
+
+    Example 1:
+
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Example 2:
+
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+    Example 3:
+
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
 
 
-        Output Format
-        Return an array of integer, representing the answer
+    Constraints:
+
+    2 <= nums.length <= 10^4
+    -10^9 <= nums[i] <= 10^9
+    -10^9 <= target <= 10^9
+    Only one valid answer exists.
 
 
-        Example Input
-        A: [2, 7, 11, 15]
-        B: 9
-
-
-        Example Output
-        [1, 2]
-
-
-        Example Explanation
-        The elements present at index 1 and index 2 add up to 9. i.e. A[1] + A[2] = 9 (1-based indexing)
-
+    Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
 */
 public class Main {
     public static void main(String[] args) {
